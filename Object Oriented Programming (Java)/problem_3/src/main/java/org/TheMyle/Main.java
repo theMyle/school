7ce57b1b3434b1e6baadf3676aa0be4fa68f31d4
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2024-08-14 19:31:50 +0800

    problem_3

---
+++ Object Oriented Programming (Java)/problem_3/src/main/java/org/TheMyle/Main.java
@@ -0,0 +1,34 @@
+package org.TheMyle;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+
+        int purchasePrice = 0;
+        int yearsInService = 0;
+        int salvageValue = 0;
+        float earlyDepreciation = 0F;
+
+        try {
+            System.out.print("Enter the purchase price of an item: ");
+            input = reader.readLine();
+            purchasePrice = Integer.parseInt(input);
+
+            System.out.print("Enter the number of years in service: ");
+            input = reader.readLine();
+            yearsInService = Integer.parseInt(input);
+
+            System.out.print("Enter the expected salvage value: ");
+            input = reader.readLine();
+            salvageValue = Integer.parseInt(input);
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
+        earlyDepreciation = (float) (purchasePrice - salvageValue) / yearsInService;
+        System.out.println("The early depreciation for this item is: " + earlyDepreciation);
+    }
+}
